6f593272e38019f3da622afe8300866d8d694476
shaibal-tiller <shaibal.tiller@gmail.com>
2023-05-09 18:50:49 +0600

    test pwa

--- components/Results.js
+++ components/Results.js
@@ -6,7 +6,7 @@ const Results = ({ results }) => {
     <div>
       <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3
     3xl:flex flex-wrap justify-center">
-      {results.map((result) => (
+      {results?.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
       </FlipMove>
